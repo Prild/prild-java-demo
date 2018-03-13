75872e3b2ba09801ad4d59b4d8bc2ff191feb862
prild <prildwang@xishiqu.com>
2018-03-13 17:07:08 +0800

    全局配置，自定义banner，自定义消息转化器，自定义SpringMVC的配置

--- prildspringboot/src/main/java/com/prild/springboot/demo/HelloApplication.java
+++ prildspringboot/src/main/java/com/prild/springboot/demo/HelloApplication.java
@@ -1,11 +1,16 @@
 package com.prild.springboot.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.nio.charset.Charset;
 
 @Controller
 @SpringBootApplication
@@ -14,10 +19,21 @@ public class HelloApplication {
     @RequestMapping("hello")
     @ResponseBody
     public String hello(){
-        return "hello world!";
+        return "hello world!你好 世界！";
     }
 
+    //自定义消息转化器
+    @Bean
+    public StringHttpMessageConverter stringHttpMessageConverter(){
+        StringHttpMessageConverter converter  = new StringHttpMessageConverter(Charset.forName("ISO-8859-1"));
+        return converter;
+    }
+
+
     public static void main(String[] args){
-        SpringApplication.run(HelloApplication.class,args);
+        //SpringApplication.run(HelloApplication.class,args);
+        SpringApplication app = new SpringApplication(HelloApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);//关闭Banner
+        app.run(args);
     }
 }
